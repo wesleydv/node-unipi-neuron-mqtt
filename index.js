1d9bccc765b5300260c91b2ac674a7c4fa33dbbf
Wesley De Vrient <wesley@digital54.com>
2017-11-27 17:51:15 +0100

    Sense double click

--- index.js
+++ index.js
@@ -11,9 +11,38 @@ let config        = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
 let mqtt          = Mqtt.connect(config.mqtt);
 let boardManager  = new BoardManager(config.boards);
 
+let state          = {};
+
 boardManager.on('update', function (id, value) {
     console.log(id + ' changed to ' + value);
     mqtt.publish('neuron/' + id, value);
+
+    value = parseInt(value);
+    let now = new Date().getTime();
+    if (!state.hasOwnProperty(id)) {
+        state[id] = {
+            value: value,
+            timeHigh: 0,
+            timeLow: 0,
+        };
+    }
+    else {
+        state[id].value = value;
+    }
+
+    let diff = 0;
+    if (value) {
+        diff = now - state[id].timeHigh;
+        state[id].timeHigh = now;
+    }
+    else {
+        diff = now - state[id].timeLow;
+        state[id].timeLow = now;
+    }
+
+    if (diff < 1000) {
+        mqtt.publish('neuron/' + id + '/double', value.toString());
+    }
 });
 
 mqtt.on('connect', function () {
